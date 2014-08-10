b3ad7ee5f5c10b5a308950b828241e3f53ce5353
CourseraStudent2014 <vtomusiak@yahoo.com>
2014-08-10 15:11:36 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -7,5 +7,9 @@ my_file <- "household_power_consumption.txt"
 ## read data into a data frame called df for two days in February
 df <- read.csv.sql(my_file, sep=";",sql = 'select * from file where Date = "1/2/2007" or Date = "2/2/2007"')
 
+## explicitly set Date and Time to date/time type with strotime function
+df$Date<- strptime(paste(df$Date,df$Time), "%d/%m/%Y %H:%M:%S")
+df$Time<- strptime(paste(df$Date,df$Time), "%d/%m/%Y %H:%M:%S")
+
 ## create a histogram with a custom title, red columns, and a custom x-axis label
 with(df, hist(Global_active_power, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)"))
