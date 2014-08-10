412f01b09b1f1b4d4db8f8a78e29b8732a500992
CourseraStudent2014 <vtomusiak@yahoo.com>
2014-08-10 14:24:59 +0200

    plot1.R
    Global Active Power histogram.

---
+++ plot1.R
@@ -0,0 +1,11 @@
+## load library in order to use sql query to read relevant data
+library(sqldf)
+
+## assign file name of the data file in the working directory
+my_file <- "household_power_consumption.txt"
+
+## read data into a data frame called df for two days in February
+df <- read.csv.sql(my_file, sep=";",sql = 'select * from file where Date = "1/2/2007" or Date = "2/2/2007"')
+
+## create a histogram with a custom title, red columns, and a custom x-axis label
+with(df, hist(Global_active_power, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)"))
